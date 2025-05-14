b917eb0231f2a246155a3f6fc202922541c3aec8
Cristopher Loyola <loyoladv@gmail.com>
2025-05-13 20:48:25 -0500

    carruselTap

--- src/app/components/FeatureCard.tsx
+++ src/app/components/FeatureCard.tsx
@@ -1,11 +1,10 @@
-
 type FeatureCardProps = {
   number: string;
   title: string;
   description?: string;
   highlight?: boolean;
   imageSrc?: string;
-  descripcionGreen?: string,
+  descripcionGreen?: string;
   className?: string;
 };
 
@@ -19,7 +18,7 @@ export default function FeatureCard({
   className
 }: FeatureCardProps) {
   return (
-   <div
+    <div
       className={`rounded-3xl p-8 shadow-md flex flex-col transition-all duration-300 border ${
         highlight ? 'bg-[#B2FA03] text-black  p-10' : 'bg-white text-black '
       }${className}`}
@@ -28,20 +27,11 @@ export default function FeatureCard({
         <div className="flex justify-between items-start ">
           <span className="text-5xl font-bold">{number}</span>
           {highlight && (
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              className="w-8 h-8"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke="currentColor"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth={2}
-                d="M17 7l-10 10M17 7h-6m6 0v6"
-              />
-            </svg>
+            <img
+              src="flecha.svg" // Cambia esta ruta a la ubicación real de tu archivo fecha.svg
+              alt="Fecha"
+              className="w-20 h-20 -mt-5"
+            />
           )}
         </div>
         <h1 className="text-[25px]  mb-2 mt-[30px] text-left">{title}</h1>
