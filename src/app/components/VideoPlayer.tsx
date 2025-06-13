aedbf3c1d28c1ef84d59405cee4d723cae399b4c
cristopher <loyoladv@gmail.com>
2025-06-13 00:51:57 -0500

    cloudflare config

--- src/app/components/VideoPlayer.tsx
+++ src/app/components/VideoPlayer.tsx
@@ -1,22 +1,18 @@
 'use client';
 
 export default function VideoPlayer() {
-
   return (
-    <div className=" max-w-7xl mx-auto mt-10">
-      {/* Título */}
+    <div className="max-w-7xl mx-auto mt-10">
       <h1 className="text-3xl md:text-[55px] font-bold text-center mb-6 text-white">
         Previa Oficial – Zeinternational
       </h1>
 
-      {/* Contenedor del video */}
       <div className="relative rounded-xl overflow-hidden" style={{ aspectRatio: '16 / 9' }}>
         <video
           className="w-full h-full object-cover"
-          src="https://www.dropbox.com/scl/fi/yoplez4lfyff72nt6pzbv/zeinVideo.mp4?rlkey=5b9md0qrw33n9caco40t9tlm1&st=rlcyb02z&dl=1"
+          src="https://pub-e0711c9681594e0f8927abb9782f801f.r2.dev/zeinVideo.mp4"
           controls
           loop
-          muted
           playsInline
           poster="/zeinternational/zein9.webp"
           preload="metadata"
