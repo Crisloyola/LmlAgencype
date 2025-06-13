75aeb281f72bd23ee52f9bcc7c655e7aa21939a3
cristopher <loyoladv@gmail.com>
2025-06-13 00:21:21 -0500

    cambio a loop y dando poster

--- src/app/components/VideoPlayer.tsx
+++ src/app/components/VideoPlayer.tsx
@@ -3,7 +3,7 @@
 export default function VideoPlayer() {
 
   return (
-    <div className=" max-w-7xl md:max-w-6xl mx-auto mt-10">
+    <div className=" max-w-7xl mx-auto mt-10">
       {/* Título */}
       <h1 className="text-3xl md:text-[55px] font-bold text-center mb-6 text-white">
         Previa Oficial – Zeinternational
@@ -15,6 +15,8 @@ export default function VideoPlayer() {
           className="w-full h-full object-cover"
           src="https://www.dropbox.com/scl/fi/yoplez4lfyff72nt6pzbv/zeinVideo.mp4?rlkey=5b9md0qrw33n9caco40t9tlm1&st=rlcyb02z&dl=1"
           controls
+          loop
+          poster="/zeinternational/zein9.webp"
           preload="metadata"
         />
       </div>
